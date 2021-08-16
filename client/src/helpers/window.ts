36e5c7b37b68f9782045621e8c8bca236c4b4e01
Harsh Pandey <hpandey793@gmail.com>
2021-08-16 10:42:18 -0400

    formatting

--- client/src/helpers/window.ts
+++ client/src/helpers/window.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -15,8 +15,8 @@ export function useWindowDimensions() {
       setWindowDimensions(getWindowDimensions());
     }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
   }, []);
 
   return windowDimensions;
